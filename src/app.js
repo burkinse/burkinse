16e806da4f01f1fb0c21cc94fb86cc7d381c6581
burkinse <burakurtuldu.se@gmail.com>
2021-10-24 21:45:33 +0200

    18 Ekim 1.ders sonu

---
+++ src/app.js
@@ -0,0 +1,14 @@
+console.log("Seni Cok Seviyorum Gizemim")
+
+let evlenmeTarihimiz = 18.04
+console.log(evlenmeTarihimiz)
+
+let sanaSeslenislerim = ["Bitanem","Hösmerimim","Lolipopum","Güzelim",15,25]
+
+
+
+console.log(sanaSeslenislerim)
+
+for (let index = 0; index < sanaSeslenislerim.length; index++) {
+    console.log("<li>"+sanaSeslenislerim[index]+"</li>") 
+}
